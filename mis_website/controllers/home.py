cb03c683f6bec9ec0cd24a9fd7e7cf6e93efeec4
Arockiadass <arockiadass@ifensys.com>
2025-03-12 22:06:32 +0530

    hsdjfdg

--- mis_website/controllers/home.py
+++ mis_website/controllers/home.py
@@ -17,16 +17,22 @@ class home_controller(http.Controller):
         today_date = fields.Datetime.now().strftime('%d-%m-%Y')
         print('XXXXXXSSSSSSS',today_date)
         notices= request.env['web.info'].sudo().search([('enable', '=', True)])
+        raw_html = ""
         for notice in notices:
             date = notice.date.strftime('%d-%m-%Y')
-            display_notice = display_notice + str(date) + ' - ' + str(notice.anounce) + '\n\n'
+            raw_html = raw_html + f"""
+                            <div style="text-align:center;">
+                                <h4 style="color:blue;"><u>{date}!</u></h2>
+                                <p><strong>{notice.anounce}</strong>.</p>
+                            </div><br/><br/>
+                            """
 
 
         print('NOFGHDFHGD',display_notice)
         vals = {
             'today_date': today_date,
             'banner': request.env['banner.info'].sudo().search([('enable', '=', True)], limit=1),
-            'notices': display_notice,
+            'notices': raw_html,
             # 'photos': request.env['program.gallery.photo'].sudo().search([]),
             # 'events': request.env['program.events'].sudo().search([]),
         }
