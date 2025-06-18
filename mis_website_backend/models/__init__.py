0274208bc4fb3ee63a5aa97f236d1a75034333bd
Arockiadass <arockiadass@ifensys.com>
2025-06-18 21:52:25 +0530

    dfdf

--- mis_website_backend/models/__init__.py
+++ mis_website_backend/models/__init__.py
@@ -8,7 +8,7 @@ from . import upload_multi_documents
 from . import e_magazine
 from . import members
 from . import web_slide
-from . import res_config_settings
+# from . import res_config_settings
 from . import mail_thread
 from . import push_notification
 # from . import res_company
